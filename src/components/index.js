0fec4ec34fa302cc61093bbed50ff873a95ad844
Ojas <dubeyojas16@gmail.com>
2023-02-15 20:33:01 +0530

    First commit

---
+++ src/components/index.js
@@ -0,0 +1,19 @@
+import Navbar from "./Navbar";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero"
+import GetStarted from "./GetStarted";
+import Footer from "./Footer"
+import FeedbackCard from "./FeedbackCard"
+import CTA from "./CTA"
+import Clients from "./Clients"
+import CardDeal from "./CardDeal"
+import Button from "./Button";
+import Business from "./Business";
+import Billing from "./Billing";
+export{
+    Navbar,Stats,Testimonials,Hero,
+    GetStarted,Footer,FeedbackCard,CTA,
+    Clients,CardDeal,Button,
+    Business,Billing
+}
